e014e6cd238312e3cc2d45f3472876316955a289
Yandall <jhonstevekill22@gmail.com>
2022-10-20 15:22:07 -0500

    cambiado form de edit seccion por form reactivo

--- src/app/dashboard/board/section/edit-section/edit-section.component.ts
+++ src/app/dashboard/board/section/edit-section/edit-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NbDialogRef } from '@nebular/theme';
 import { Subject } from 'rxjs';
 import { Section } from 'src/app/shared/types';
@@ -9,26 +10,34 @@ import { Section } from 'src/app/shared/types';
   styleUrls: ['./edit-section.component.scss'],
 })
 export class EditSectionComponent implements OnInit {
-  section: Partial<Section> = {};
-  editConfig: { [key: string]: any } = {
-    title: '',
-    accentColor: '',
-  };
+  sectionForm: FormGroup;
+  section: Section;
 
   onSave = new Subject<Section>();
 
   constructor(private dialogRef: NbDialogRef<EditSectionComponent>) {}
 
   ngOnInit(): void {
-    this.editConfig = { ...this.section.config };
+    this.initForm();
+    this.sectionForm.setValue({ ...this.section.config });
   }
 
   save() {
-    this.onSave.next({ ...(this.section as Section), config: this.editConfig });
+    this.onSave.next({ ...this.section, config: this.sectionForm.value });
     this.dialogRef.close();
   }
 
   close() {
     this.dialogRef.close();
+  }
+
+  initForm() {
+    this.sectionForm = new FormGroup({
+      title: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      accent: new FormControl(''),
+    });
   }
 }

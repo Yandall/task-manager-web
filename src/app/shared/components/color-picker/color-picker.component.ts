a000dcf91e96d33657e1173e46fe5f6ba515c3ac
Yandall <jhonstevekill22@gmail.com>
2022-10-20 15:15:10 -0500

    Componente color picker compatible con reactive forms

--- src/app/shared/components/color-picker/color-picker.component.ts
+++ src/app/shared/components/color-picker/color-picker.component.ts
@@ -1,4 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 import { NbDialogService } from '@nebular/theme';
 import { first } from 'rxjs';
 import { EditDialogComponent } from './edit-color-picker/edit-color-picker.component';
@@ -7,17 +19,37 @@ import { EditDialogComponent } from './edit-color-picker/edit-color-picker.compo
   selector: 'color-picker',
   templateUrl: './color-picker.component.html',
   styleUrls: ['./color-picker.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ColorPickerComponent),
+      multi: true,
+    },
+  ],
 })
-export class ColorPickerComponent implements OnInit {
-  @Input()
-  value = '#FF7128';
-
-  @Output()
-  color = new EventEmitter<string>();
+export class ColorPickerComponent implements ControlValueAccessor {
+  public readonly colorControl = new FormControl();
 
   constructor(private dialogService: NbDialogService) {}
 
-  ngOnInit(): void {}
+  writeValue(value: string | null): void {
+    this.colorControl.setValue(value);
+    this._onChange(value);
+  }
+
+  private _onChange(value: string | null) {}
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+
+  private _onTouched() {}
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    if (isDisabled) this.colorControl.disable();
+    else this.colorControl.enable();
+  }
 
   edit() {
     const compRef = this.dialogService.open(EditDialogComponent, {
@@ -27,12 +59,16 @@ export class ColorPickerComponent implements OnInit {
     });
     const subscription = compRef.componentRef.instance.colorSelected.subscribe(
       (value) => {
-        this.value = value;
-        this.color.emit(value);
+        this.writeValue(value);
+        this._onTouched();
       }
     );
     compRef.onClose.pipe(first()).subscribe(() => {
       subscription.unsubscribe();
     });
+  }
+
+  get value() {
+    return this.colorControl.value;
   }
 }
